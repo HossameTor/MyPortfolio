4ea9629944b4286c4121e302873c60cc549fd3d5
HossameTorCHi <hossame.torchi@um5r.ma>
2022-11-19 19:30:48 +0100

    volounteering $ experiences command

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,27 @@
 function talk() {
     var know = {
-        "man": "Who are you <br>  dshbfvadskhugv",
-        "How are you": "Good :)",
+        "man": " whoami: Get to know me . <br>"+  
+        " github: Get the link to my github profile.<br> "+
+        "volunteering & experiences : Discover all my professional and volouneering experiences",
+        "whoami": " I am 21 yo. I am from Casablanca. I am actually a software engineering "+ 
+        "student at High National School for Computer Science and Systems Analysis - ENSIAS in Rabat, "+
+        "a DevOps enthusiast and intersted by cloud and virtualisation.",
         "What can i do for you": "Search CodeWithRandom on Google>>Thank Me Later",
-        "Your followers":
-            "I have my family of 200000 members, i don't have follower ,have supportive Famiy ",
-        ok: "Thank You So Much ",
+        "volunteering & experiences":
+            "wibday: 2 moonths internship as a full stack developer<br>"+
+            "IEEE ENSIAS Student branch: Vice chairman and technincal manager"+
+            " ",
+
+        "github": " <a href='https://github.com/HossameTor' target='blank' style='text-decoration:none'>Click me</a> ",
         Bye: "Okay! Will meet soon..",
     };
     var user = document.getElementById("userBox").value;
     document.getElementById("chatLog").innerHTML = user + "<br>";
     if (user in know) {
-        document.getElementById("chatLog").innerHTML = know[user] + "<br>";
+        document.getElementById("chatLog").innerHTML = "<span style='color:green;'>HossameTorchi@portfolio:</span><span style='color:blue;'>~$</span>" + know[user] + "<br>";
     } else {
         document.getElementById("chatLog").innerHTML =
-            "Sorry,I didn't understand <br>";
+        "<span style='color:green;'>HossameTorchi@portfolio:</span><span style='color:blue;'>~$</span> "  + user + ": command not found <br>";
     }
 }
 
